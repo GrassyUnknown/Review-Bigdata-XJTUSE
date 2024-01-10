bb77266e0823949ecb87bbb711e4b2341c12d6dc
未晞 <lingchen47@outlook.com>
2024-01-10 10:41:33 +0800

    完成评论后端

---
+++ SpringBoot/hotel/src/main/java/com/hotel/mapper/ReviewMapper.java
@@ -0,0 +1,12 @@
+package com.hotel.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import com.hotel.pojo.entity.Review;
+import java.util.List;
+
+@Mapper
+public interface ReviewMapper {
+    @Select("select * from review where business_id=#{businessId}")
+    List<Review> getReviewList(String businessId);
+}
---
+++ SpringBoot/hotel/src/main/java/com/hotel/service/ReviewService.java
@@ -0,0 +1,8 @@
+package com.hotel.service;
+
+import java.util.List;
+
+import com.hotel.pojo.entity.Review;
+public interface ReviewService {
+    List<Review> getReviewList(String businessId);
+}
